03dbb750425de15b5bfbc14da11148828f41c402
AJMALSER <140539033+AJMALSER@users.noreply.github.com>
2025-08-05 22:04:14 +0530

    Update menu-new.js

--- plugins/menu-new.js
+++ plugins/menu-new.js
@@ -39,7 +39,7 @@ cmd({
             forwardingScore: 999,
             isForwarded: true,
             forwardedNewsletterMessageInfo: {
-                newsletterJid: '120363348739987203@newsletter',
+                newsletterJid: '3@newsletter',
                 newsletterName: config.AJMAL,
                 serverMessageId: 143
             }
